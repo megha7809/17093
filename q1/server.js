e80f1f8678816774b1ec4d45372c545156a28110
megha7809 <meghak774@gmail.com>
2025-09-20 05:35:58 +0530

    testing123

--- q1/server.js
+++ q1/server.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const express = require('express');
+const app = express();
+const port = 4000;
+app.get('/user',(req,res)=>{
+    res.end("getting data");
+})
+app.post('/user',(req,res)=>{
+    res.end("adding new data");
+})
+app.put('/user/:id',(req,res)=>{
+    console.log(req.params.id)
+    res.end("updation");
+})
+app.delete('/user/:id',(req,res)=>{
+    
+    res.end("Deletion");
+})
+app.listen(port,()=>console.log(`server running on ${port}`));
